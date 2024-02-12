8434211c0b02a49294d04a276f923593a6681fbb
Th35py27 <nvallamkondu27@gmail.com>
2024-02-12 10:10:56 -0800

    backend things

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ from __init__ import app, db, cors  # Definitions initialization
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
 from api.searchstocks import search_api
+from api.cryptocode import crypto_api
 # database migrations
 from model.users import initUsers
 from model.players import initPlayers
+#from model.crypto import init_data
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
@@ -40,6 +42,7 @@ app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
 app.register_blueprint(search_api)
+app.register_blueprint(crypto_api)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
--- model/crypto.py
+++ model/crypto.py
@@ -8,86 +8,27 @@ from __init__ import app, db
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
-class Player(db.Model):
-    __tablename__ = 'players'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=False, nullable=False)
-    uid = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), unique=False, nullable=False)
-    tokens = db.Column(db.Float, nullable=False)
-
-    def __init__(self, name, uid, tokens, password="123qwerty"):
-        self.name = name
-        self.uid = uid
-        self.set_password(password)
-        self.tokens = tokens
-
-    @property
-    def password(self):
-        return self._password[0:10] + "..."
-
-    def set_password(self, password):
-        self._password = generate_password_hash(password, "pbkdf2:sha256", salt_length=10)
-
-    def is_password(self, password):
-        return check_password_hash(self._password, password)
-
-    def __str__(self):
-        return json.dumps(self.read())
-
-    def create(self):
-        try:
-            db.session.add(self)
-            db.session.commit()
-            return self
-        except IntegrityError:
-            db.session.remove()
-            return None
-
-    def read(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "uid": self.uid,
-            "tokens": self.tokens
-        }
-
-    def update(self, dictionary):
-        for key in dictionary:
-            if key == "name":
-                self.name = dictionary[key]
-            if key == "uid":
-                self.uid = dictionary[key]
-            if key == "password":
-                self.set_password(dictionary[key])
-            if key == "tokens":
-                self.tokens = dictionary[key]
-        db.session.commit()
-        return self
-
-    def delete(self):
-        player = self
-        db.session.delete(self)
-        db.session.commit()
-        return player
-
-
 class Transaction(db.Model):
     __tablename__ = 'transactions'
     
-    uid = db.Column(db.String(255), db.ForeignKey('players.uid'), nullable=False)
+    transaction_id = db.Column(db.Float,primary_key=True)
+    user_id = db.Column(db.Float, db.ForeignKey('users.id'), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     transaction_type = db.Column(db.String(50), nullable=False)
+    executed_price = db.Column(db.Float, nullable=False)
 
-    def __init__(self, uid, amount, transaction_type):
-        self.uid = uid
+
+    def __init__(self,user_id, amount, transaction_type,executed_price):
+        self.user_id = user_id
         self.amount = amount
         self.transaction_type = transaction_type
+        self.executed_price = executed_price
 
     def create(self):
         try:
+            print(self.user_id)
+            print(self.amount)
+
             db.session.add(self)
             db.session.commit()
             return self
@@ -98,22 +39,8 @@ class Transaction(db.Model):
 
 def init_data():
     with app.app_context():
-        db.create_all()
         
-        players = [
-            Player(name='Azeem Khan', uid='azeemK', tokens=45),
-            Player(name='Ahad Biabani', uid='ahadB', tokens=41),
-            Player(name='Akshat Parikh', uid='akshatP', tokens=40),
-            Player(name='Josh Williams', uid='joshW', tokens=38),
-            Player(name='John Mortensen', uid='johnM', tokens=35)
-        ]
-
-        for player in players:
-            try:
-                player.create()
-            except IntegrityError:
-                db.session.remove()
-                print(f"Records exist, duplicate uid, or error: {player.uid}")
+        db.create_all()
 
         transactions = [
             Transaction(uid='azeemK', amount=10.5, transaction_type='buy'),
@@ -130,5 +57,5 @@ def init_data():
                 print(f"Failed to create transaction: {transaction}")
 
 
-if __name__ == "__main__":
-    init_data()
+#if __name__ == "__main__":
+    #init_data()
